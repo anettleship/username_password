925143bb91d5ce0493ce99122923b52c1988c774
Nettleship <nettlesh@f8ffc20d08ec.ant.amazon.com>
2020-08-19 22:15:02 +0100

    store and retrive password working from keychain

--- passwordings.py
+++ passwordings.py
@@ -5,3 +5,11 @@ def user_input(message, hide):
     """
     inputstr = input(message)
     return inputstr
+
+def get_user_credentials(namespace):
+    """
+    Handles saving username and password to the system keychain and retrieving them, using a namespace for the specific
+    application to test if username and password are already stored. A single username and password pair is supported for each
+    namespace. To use in your script, make up a namespace for your script that won't be in use already.
+    """
+    
